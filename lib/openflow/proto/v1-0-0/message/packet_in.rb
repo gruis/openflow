ea32d6b7b06ea85f34ba4fbba4f0150b602f47c1
Caleb Crane <github@simulacre.org>
2013-04-14 22:36:32 +0900

    use enum in PacketIn

--- lib/openflow/proto/v1-0-0/message/packet_in.rb
+++ lib/openflow/proto/v1-0-0/message/packet_in.rb
@@ -3,34 +3,13 @@ class OpenFlow
     module V0x01
       class Message
         class PacketIn < BinData::Record
-
-          class PacketInReason < BinData::BasePrimitive
-            REASONS = [
-              :no_match,
-              :action
-            ]
-
-            def sensible_default
-              nil
-            end
-
-            def read_and_return_value(io)
-              type = io.readbytes(1).unpack("C")[0]
-              REASONS[type]
-            end
-
-            def value_to_binary_string(value)
-              [REASONS.index(value)].pack("C")
-            end
-          end # class::PacketInReason < BinData::BasePrimitive
-
           include Base
 
           endian :big
           uint32 :buffer_id
           uint16 :total_len
           uint16 :in_port
-          packet_in_reason :reason
+          enum :reason, :type => :uint8, :values => [:no_match, :action]
           uint8 :pad
           string :data, :read_length => :total_len
 
